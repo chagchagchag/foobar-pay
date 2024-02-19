ab190c1ebdf691519eae7d235068a3a5d0c28dea
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-19 14:33:53 +0900

    first status

---
+++ src/main/java/io/project/chagchagchag/foobarpay/FoobarPayApplication.java
@@ -0,0 +1,13 @@
+package io.project.chagchagchag.foobarpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoobarPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoobarPayApplication.class, args);
+	}
+
+}
